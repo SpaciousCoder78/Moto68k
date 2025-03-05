bd6135040ea597d3a102dc5d5d3d7c6e8ef1d1a6
Aryan Karamtoth <88923986+SpaciousCoder78@users.noreply.github.com>
2025-03-05 21:15:04 +0530

    Update registers.h

--- m68k/registers/registers.h
+++ m68k/registers/registers.h
@@ -11,12 +11,35 @@
 
 //Definining data registers of 68k
 struct datareg{
-  unsigned int d0;
-  unsigned int d1;
-  unsigned int d2;
-  unsigned int d3;
-  unsigned int d4;
-  unsigned int d5;
-  unsigned int d6;
-  unsigned int d7;
+  unsigned char d0;
+  unsigned char d1;
+  unsigned char d2;
+  unsigned char d3;
+  unsigned char d4;
+  unsigned char d5;
+  unsigned char d6;
+  unsigned char d7;
 }datareg;
+
+//address registers
+struct addreg{
+  unsigned char a0;
+  unsigned char a1;
+  unsigned char a2;
+  unsigned char a3;
+  unsigned char a4;
+  unsigned char a5;
+  unsigned char a6;
+}
+
+//stack pointers
+struct stackpointers{
+  unsigned char a7u; //user stack pointer
+  unsigned char a7s; //supervisor stack pointer
+}
+
+//program counter
+struct programcounter{
+  unsigned char pc;
+}
+#endif
