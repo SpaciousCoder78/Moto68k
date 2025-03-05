1826f417d1150b0a09501b5b01f36d0200f67221
Aryan Karamtoth <88923986+SpaciousCoder78@users.noreply.github.com>
2025-03-05 20:28:21 +0530

    Create registers.h

---
+++ m68k/registers/registers.h
@@ -0,0 +1,22 @@
+/*
+* File Name: registers.h
+* Author: Aryan Karamtoth
+* Date Created: 05 Mar 2025
+* Last Modified: 05 Mar 2025
+* License: MIT
+*/
+
+#ifndef REGISTERS_H
+#define REGISTERS_H
+
+//Definining data registers of 68k
+struct datareg{
+  unsigned int d0;
+  unsigned int d1;
+  unsigned int d2;
+  unsigned int d3;
+  unsigned int d4;
+  unsigned int d5;
+  unsigned int d6;
+  unsigned int d7;
+}datareg;
